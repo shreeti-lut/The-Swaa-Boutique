0ebf7feb5fa1356acf782f2d01c1186c15eead57
S.Shre <Automation Developer>
2026-05-18 10:59:29 +0300

    amendment is script and parallex style

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 /* =========================Hamburger Menu Section========================== */
 
@@ -73,80 +72,4 @@ document.addEventListener("DOMContentLoaded", () =>  {
             document.getElementById('signupForm').classList.remove("active");
             document.getElementById('forgetForm').classList.add("active");
             
-=======
-
-/* =========================Hamburger Menu Section========================== */
-
-
-const hamburger = document.getElementById("hamburger");
-
-const menu = document.getElementById("menu");
-
-hamburger.addEventListener("click", function(){
-
-    menu.classList.toggle("active");
-
-});
-
-/* =========================FAQ Section========================== */
-document.addEventListener("DOMContentLoaded", () =>  {
-    const faqContainers = document.querySelector('.faq-content');
-   
-    faqContainers.addEventListener("click",  (e) => {
-        const faqQuestion = e.target.closest('.faq-question');
-        if(!faqQuestion) return;
-  
-        const faqItem = faqQuestion.parentElement;  
-        const faqAnswer = faqItem.querySelector('.faq-answer');
-        const icon = faqQuestion.querySelector('i');
-
-
-        /*       Toggle the display of the answer and the icon       ================================================== */
-        
-        icon.classList.toggle('fa-minus');
-        icon.classList.toggle('fa-plus');
-
-        /* Toggle visibility of other answers and reset icons       ================================================== */
-        faqAnswer.classList.toggle('open');
-        const allFaqItems = faqContainers.querySelectorAll('.faq-group');
-        allFaqItems.forEach(item => {
-            if(item !== faqItem){
-                const answer = item.querySelector('.faq-answer');
-                const itemIcon = item.querySelector('.faq-question i'); 
-
-                answer.classList.remove('open');
-                itemIcon.classList.remove('fa-minus');
-                itemIcon.classList.add('fa-plus');
-               
-            }                       
-        });
-
-
-        
-});
-});
-
- // Form switching functionality
-        function showSignup() {
-            document.getElementById('loginForm').classList.remove("active");
-            document.getElementById('signupForm').classList.add("active");
-            document.getElementById('forgetForm').classList.remove("active");
-            
-          
-        }
-
-        function showLogin() {
-            document.getElementById('loginForm').classList.add("active");
-            document.getElementById('signupForm').classList.remove("active");
-            document.getElementById('forgetForm').classList.remove("active");
-            
-         
-        }
-
-        function showForgotPassword() {
-            document.getElementById('loginForm').classList.remove("active");
-            document.getElementById('signupForm').classList.remove("active");
-            document.getElementById('forgetForm').classList.add("active");
-            
->>>>>>> 751db64130e9ef609462961f9bf864eb1d2abfa0
         }
